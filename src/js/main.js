5848e1960692aa419f383f50259cb9803658b7b4
Max <sss123123111@yandex.ru>
2023-09-25 17:29:38 +0300

    add slider, form

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,5 @@
-import { changeMenu } from '../js/components/burber-menu';
 import { initializationSliders } from '../js/components/sliders';
 
 const header = document.querySelector('.header');
 
-changeMenu();
 initializationSliders();
-
-window.onscroll = () => {
-	const top = window.scrollY;
-	if (top >= 100) header.classList.add('active');
-	else header.classList.remove('active');
-};
